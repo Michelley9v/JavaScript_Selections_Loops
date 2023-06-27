f4cae0307f0fc6d17a0fad15b77b93c5644e9dad
Michelle Hoffmann <michelley9v@yahoo.com>
2023-06-27 00:42:03 -0500

    Loops

--- app.js
+++ app.js
@@ -2,6 +2,60 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+for (let i = 1; i <= 100; 1 += 2 ) {
+if (i % 1 ==0) {
+    console.log(i)
+} 
+else {
+    if (i % 1 )}
+}
+
+for (let i = 0; i < 100; i++){
+if (i % 2 == 0) {
+    continue;
+} else {
+    console.log(i);
+}
+}
+let i = 1;
+
+while (i < 100){
+    if ( i % 2 ! == 0) {
+        console.log(i);
+    }
+}
+
+
+
+
+
+
+
+
+
 
 // Exercise 2 Section
-console.log("EXERCISE 2:\n==========\n");
+console.log("EXERCISE 2:\nFizzBuzz\n");
+
+for (let i = 1; i < 100; i++) {
+    console.log("i");
+if (i % 3 == 0 && i % 5 == 0) {
+        console.log(i, "FizBuzz");
+    } else {
+        if (i % 3 == 0) {
+            console.log(i, "Fizz");
+        }
+        if (i % 5 == 0) {
+            console.log(i, "Buzz");
+        }
+
+        for ( let i = 1; i <= 100; i ++) {
+            if (i % 15 == 0){
+                console.log(i, "Fizzbuzz");
+        }else if ( i % 5 == 0) {
+            console.log(i, "Buzz");
+    }else if (i % 3 == 0 ) {
+        console.log(i, "Fizz");
+    }
+ }
+ 
